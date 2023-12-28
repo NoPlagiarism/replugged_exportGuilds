76819e7bc2ce3ff30a78e070e0a82827b0311121
NoPlagiarism <37241775+NoPlagiarism@users.noreply.github.com>
2023-12-29 03:44:13 +0500

    Init

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,41 @@
-import { Injector, Logger, webpack } from "replugged";
+import { Injector, Logger, settings, common } from "replugged";
+import { Guild } from "discord-types/general";
 
 const inject = new Injector();
-const logger = Logger.plugin("PluginTemplate");
+const logger = Logger.plugin("xyz.noplagi.exportGuilds");
 
-export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) => {
-      name: string;
-    };
-  }>(webpack.filters.byProps("getChannel"));
+interface GuildExport {
+  id: string;
+  name: string;
+  description?: string;
+  icon?: string;
+}
 
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      logger.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
-    });
+interface SettingsData {
+  guilds: GuildExport[];
+  lastUpdated: number;
+}
+
+interface SettingsInt {
+  lastUpdated: number;
+  data: SettingsData;
+}
+
+const cfg = await settings.init<SettingsInt>("xyz.noplagi.exportGuilds");
+
+function transferGuild(old: Guild): GuildExport {
+  return { id: old.id, name: old.name, description: old.description, icon: old.icon };
+}
+
+export function start(): void {
+  const guilds = common.guilds.getGuilds();
+  let guildsToExport = Array<GuildExport>();
+  for (const key in guilds) {
+    const guild = transferGuild(guilds[key]);
+    guildsToExport.push(guild);
   }
+  cfg.set("data", { guilds: guildsToExport, lastUpdated: Math.floor(Date.now() / 1000) });
+  logger.log("Updated data");
 }
 
 export function stop(): void {
